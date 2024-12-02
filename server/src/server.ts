0891654058522069e44240efe2196993ff9a0789
Nicholas Poh <tspoh@willowglen.com.my>
2024-12-02 13:19:44 +0800

    Added video streaming tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ import taskRoutes from './routes/tasks';
 import fetchDemoRoutes from './routes/fetchDemo';
 
 import problemDetailsResponseMiddleware from "./middlewares/problem-details-response";
+import axios from "axios";
+import https from "https";
 
 dotenv.config();
 
@@ -22,6 +24,47 @@ const initServer = async () => {
 
   app.get("/sip", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "./pages/webrtcsip_multi_mics.html"));
+  });
+
+  app.get("/nxcctv.html", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "./pages/nxcctv.html"));
+  });
+
+  app.get("/hlsplayer.html", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "./pages/hlsplayer.html"));
+  });
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.get("/stream", async (req: Request, res: Response) => {
+    const agent = new https.Agent({
+      rejectUnauthorized: false
+    });
+    const url = req.query.authenticatedUrl as string;
+    const token = req.query.token as string;
+    await axios.get(url, {
+      httpsAgent: agent,
+      headers: {
+        "Accept": "video/webm",
+        "Content-Type": "video/webm",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Authorization": `Bearer ${token}`
+      },
+      responseType: "stream"
+    })
+      .then(response => {
+        response.data.pipe(res);
+      })
+      .catch((error) => {
+        if (error?.response?.status === undefined)
+          res.status(500).json(error);
+        else if (error.response.status === 401)
+          res.send("Invalid token");
+        else
+          res.status(500).json(error);
+      });
+
   });
 
   app.use('/form', formRoutes);
